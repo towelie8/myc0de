74079b9920d992d98a17c137e1a513b7ebb8f0d9
towelie8 <maximilian6695@web.de>
2024-08-26 15:24:30 +0200

    fixed grammar

--- src/app/thank-you/page.jsx
+++ src/app/thank-you/page.jsx
@@ -2,13 +2,13 @@ import { SimpleLayout } from '@/components/SimpleLayout'
 
 export const metadata = {
   title: 'Du hast abonniert!',
-  description: 'Danke für das Abonieren des Newsletters.',
+  description: 'Danke für das Abonnieren des Newsletters.',
 }
 
 export default function ThankYou() {
   return (
     <SimpleLayout
-      title="Danke fürs Abonieren des Newsletters!"
+      title="Danke fürs Abonnieren des Newsletters!"
       intro="Ich werde dir eine E-Mail senden, jedes Mal wenn ich einen neuen Blogpost veröffentliche, ein neues Projekt freigebe oder etwas Interessantes zu teilen habe, von dem ich denke, dass du davon hören möchtest."
     />
   )
