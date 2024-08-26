36aa0857c2a4894b7af08f2e5549a77519eb0919
towelie8 <maximilian6695@web.de>
2024-08-26 15:12:35 +0200

    removed X and LinkedIn

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -77,18 +77,18 @@ export default function About() {
         </div>
         <div className="lg:pl-20">
           <ul role="list">
-            <SocialLink href="#" icon={XIcon}>
+           {/* <SocialLink href="#" icon={XIcon}>
               Follow on X
             </SocialLink>
-          {/*}  <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+         }  <SocialLink href="#" icon={InstagramIcon} className="mt-4">
               Follow on Instagram
   </SocialLink>*/}
             <SocialLink href="#" icon={GitHubIcon} className="mt-4">
               Follow on GitHub
             </SocialLink>
-            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+          {/*  <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
               Follow on LinkedIn
-            </SocialLink>
+</SocialLink>*/}
             <SocialLink
               href="mailto:maximilian6696@web.d"
               icon={MailIcon}
